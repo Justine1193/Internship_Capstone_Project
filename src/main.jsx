107f512ac4035c7f3c7972552a4c835d20bea52e
JohnLeeAeolus <johnleeaeolusc@gmail.com>
2025-04-23 22:09:57 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 import LandingPage from './Pages/LandingPage/LandingPage.jsx';
 import SignInPage from './Components/Register/SignIn/SignInPage.jsx';
-import SignUpForm from './Components/Register/SignUp/CreateAccount.jsx';
+import SignUpForm from './Components/Register/SignUp/CreateAccount.jsx'; // Correct import name
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,8 +15,30 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<CreateAccount />} />
+        <Route path="/signup" element={<SignUpForm />} /> {/* Fixed usage */}
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
+);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import LandingPage from './Pages/LandingPage/LandingPage.jsx';
+import SignInPage from './Components/Register/SignIn/SignInPage.jsx';
+import SignUpForm from './Components/Register/SignUp/CreateAccount.jsx'; // Correct import name
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpForm />} /> {/* Fixed usage */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
